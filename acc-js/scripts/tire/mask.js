0ac606e63ab570be76229e794751a5bf39ca866f
wmacc <77104304+wmacc@users.noreply.github.com>
2021-07-03 09:12:43 -0500

    Delete acc-js directory

--- acc-js/scripts/tire/mask.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("tire_size").addEventListener("input", function (e) {
-  var x = e.target.value.replace(/\D/g, "").match(/(\d{0,3})(\d{0,2})(\d{0,2})/);
-  e.target.value = x[1] ? "P" + x[1] + (x[2] ? "/" + x[2] : "") + (x[3] ? "R" + x[3] : "") : "";
-});
