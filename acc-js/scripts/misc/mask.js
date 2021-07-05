e2c4e663bb1581a45dd8c5054708beea13d99ffe
wmacc <77104304+wmacc@users.noreply.github.com>
2021-07-05 15:05:37 -0500

    Update mask.js

--- acc-js/scripts/misc/mask.js
+++ acc-js/scripts/misc/mask.js
@@ -3,6 +3,8 @@ document.getElementById('mileage').addEventListener('input', function (e) {
     e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : "";
 });
 
+document.getElementById('mileage').addEventListener('blur', function (e) { if (e.target.value[0] == 0 && e.target.value[1] == 0) { var x = e.target.value.replace(/^0+/, '').match(/(\d{1})(\d{3})/); e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : ""; } else if (e.target.value[0] == 0) { var x = e.target.value.replace(/^0+/, '').match(/(\d{2})(\d{3})/); e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : ""; } else { var x = e.target.value.replace(/\D/g, "").match(/(\d{3})(\d{3})/); e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : ""; }});
+
 document.getElementById("upc1").addEventListener("input", function (e) {
   var x = e.target.value.replace(/\D/g, "").match(/(\d{0,1})(\d{0,5})(\d{0,5})(\d{0,1})/);
   e.target.value = x[1] ? x[1] + (x[2] ? "-" + x[2] : "") + (x[3] ? "-" + x[3] : "") + (x[4] ? "-" + x[4] : "") : "";
