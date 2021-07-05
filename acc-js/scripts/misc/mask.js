18efbcb1b16a7e20142c556160976fb46ca5895e
wmacc <77104304+wmacc@users.noreply.github.com>
2021-07-05 14:14:47 -0500

    Update mask.js

--- acc-js/scripts/misc/mask.js
+++ acc-js/scripts/misc/mask.js
@@ -1,12 +1,9 @@
 document.getElementById('mileage').addEventListener('blur', function (e) {
-  if (e.target.value[0] == 0 && e.target.value[1] == 0) {
-    var x = e.target.value.replace(/^0+/, '').match(/(\d{1})(\d{3})/);
-    e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : "";
-  } else if (e.target.value[0] == 0) {
-    var x = e.target.value.replace(/^0+/, '').match(/(\d{2})(\d{3})/);
+  if (e.target.value[0] != 0) {
+     var x = e.target.value.replace(/\D/g, "").match(/(\d{3})(\d{3})/);
     e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : "";
   } else {
-    var x = e.target.value.replace(/\D/g, "").match(/(\d{3})(\d{3})/);
+    var x = e.target.value.replace(/^0+/, '').match(/(\d{2})(\d{3})/);
     e.target.value = x[1] ? x[1] + (x[2] ? "," + x[2] : "") : "";
   }
 });
